3765fd80707872d9726ade1e3a8f0680cd24d079
朱建新 <zhujianxin@chandashi.com>
2018-08-27 20:02:44 +0800

    修改实例

--- src/components/test.tsx
+++ src/components/test.tsx
@@ -1,5 +1,5 @@
 import Vue, { CreateElement } from 'vue'
-import { Component, Prop } from 'vue-property-decorator'
+import { Component, Prop, Model } from 'vue-property-decorator'
 import { VueComponent } from 'vue-tsx-helper'
 
 export interface IProps {
@@ -15,12 +15,21 @@ export default class TestComponent extends VueComponent<IProps> {
     @Prop()
     public click: any
 
+    @Model('input', {
+        default: '2323232'
+    })
+    public value!: string
+
     private msg: string = 'init msg'
 
     public testClick() {
         this.msg = 'click img'
         this.$emit('click')
         this.click()
+    }
+
+    public onChange(v: any) {
+        console.log(v)
     }
 
     public beforeUpdate() {
@@ -32,6 +41,13 @@ export default class TestComponent extends VueComponent<IProps> {
             <div>
                 <div onClick={this.testClick}>
                     {this.m} {this.msg}
+                    <input
+                        type='text'
+                        v-model={this.value}
+                        value={this.value}
+                        onChange={this.onChange}
+                    />
+                    {this.value}
                 </div>
             </div>
         )
